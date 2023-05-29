b1ea8aa07516140b7782d89b7791c622285da01f
ChavinduShirantha <chavindushirantha@gmail.com>
2023-05-29 13:54:50 +0530

    Create Login Form

---
+++ src/lk/playTech/liveChat/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package lk.playTech.liveChat.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * @author : Chavindu
+ * created : 5/29/2023-1:47 PM
+ **/
+
+public class LoginFormController {
+    @FXML
+    private JFXTextField txtUserName;
+    @FXML
+    private JFXButton btnLogin;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+}
